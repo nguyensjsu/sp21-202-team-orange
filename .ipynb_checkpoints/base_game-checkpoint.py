04f218389143a4f3055623c4dbb5c45634b5d21f
William Su <wilson289296@gmail.com>
2021-05-14 13:59:59 -0700

    Partial attribute consolidation for bullet physics. Bullets now collide with floor.

--- .ipynb_checkpoints/base_game-checkpoint.py
+++ .ipynb_checkpoints/base_game-checkpoint.py
@@ -26,23 +26,11 @@ if not audio_compat:
         os.path.join("images", "Assets_Gun+Silencer.mp3"))
     MAIN_BG_SOUND = pygame.mixer.Sound(os.path.join("images", "Main_BG.ogg"))
     GAME_BG_SOUND = pygame.mixer.Sound(os.path.join("images", "Game_BG.ogg"))
-
 # Backgrounds
 GAME_IMAGE = pygame.transform.scale(pygame.image.load(
     os.path.join("images", "BG.png")), (WIDTH, HEIGHT))
 MAIN_IMAGE = pygame.transform.scale(pygame.image.load(
     os.path.join("images", "background.jpg")), (WIDTH, HEIGHT))
-
-#explosion animation 
-#load the images into the list
-explosion_anim = {}
-explosion_anim['lg'] = []
-for i in range(9):
-    file = 'regularExplosion0{}.png'.format(i)
-    img = pygame.image.load(os.path.join("images", file))
-    img.set_colorkey(BLACK)
-    img_lg = pygame.transform.scale(img, (72, 72))
-    explosion_anim['lg'].append(img_lg)
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 #MENUSCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -53,13 +41,10 @@ PLAYER2_HIT = pygame.USEREVENT + 2
 pygame.display.set_caption("Worms")
 use_snow = True
 snow_list = []
-# control fps
-clock = pygame.time.Clock()
 
 
-class Bullet(pygame.sprite.Sprite):
+class Bullet(object):
     def __init__(self, x, y, radius, color):
-        pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.radius = radius
@@ -67,22 +52,19 @@ class Bullet(pygame.sprite.Sprite):
         self.color = color
         self.hitbox = pygame.Rect(x, y, self.dia, self.dia)
         self.hit = False
-        self.power = 0
-        self.angle = 0
-        self.time = 0
 
     def draw(self, win):
         pygame.draw.circle(win, (0, 0, 0), (self.x, self.y), self.radius)
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius-1)
 
-    #@staticmethod
+    @staticmethod
     # bullet projectile function
-    def projectilePath(self, x, y):#, x, y):#, power, angle, time):
-        velX = math.cos(self.angle) * self.power
-        velY = math.sin(self.angle) * self.power
+    def projectilePath(x, y, power, angle, time):
+        velX = math.cos(angle) * power
+        velY = math.sin(angle) * power
 
-        distX = velX * self.time
-        distY = (velY * self.time) + ((-4.9 * (self.time)**2)/2)
+        distX = velX * time
+        distY = (velY * time) + ((-4.9 * (time)**2)/2)
 
         newX = round(distX + x)
         newY = round(y - distY)
@@ -90,34 +72,8 @@ class Bullet(pygame.sprite.Sprite):
         return newX, newY
 
 
-class Explosion(pygame.sprite.Sprite):
-    def __init__(self, center, size):
-        pygame.sprite.Sprite.__init__(self)
-        self.size = size
-        self.image = explosion_anim[self.size][0]
-        self.rect = self.image.get_rect()
-        self.rect.center = center
-        self.frame = 0
-        self.last_update = pygame.time.get_ticks()
-        self.frame_rate = 50
-
-    def update(self):
-        now = pygame.time.get_ticks()
-        if now - self.last_update > self.frame_rate:
-            self.last_update = now
-            self.frame += 1
-            if self.frame == len(explosion_anim[self.size]):
-                self.kill()
-            else:
-                center = self.rect.center
-                self.image = explosion_anim[self.size][self.frame]
-                self.rect = self.image.get_rect()
-                self.rect.center = center
-
-     
-class Player(pygame.sprite.Sprite):
+class Player:
     def __init__(self, x, y, image):
-        pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.hp = 100
@@ -127,7 +83,6 @@ class Player(pygame.sprite.Sprite):
         self.alpha = 255
         self.speed = 5
         self.fuel = 100
-        self.shield = 100
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.bullet = Bullet(self.x + self.width // 2,
                              self.y + self.height // 2, 5, RED)
@@ -150,12 +105,10 @@ class Player(pygame.sprite.Sprite):
         # self.bullet = None
         self.bullet.x = self.x + self.width // 2
         self.bullet.y = self.y + self.height // 2
-        self.bullet.time = 0
 
     def new_bullet(self):
         self.bullet = Bullet(self.x + self.width // 2,
                              self.y + self.height // 2, 5, RED)
-        return self.x, self.y
 
 
 def handle_bullets(active_player: Player, dormant_player: Player, isPlayer1: bool):
@@ -199,10 +152,10 @@ def draw_winner(text):
     WIN.blit(draw_text, (WIDTH/2-draw_text.get_width() /
              2, HEIGHT/2 - draw_text.get_height()/2))
     pygame.display.update()
-    pygame.time.delay(1500)
+    pygame.time.delay(2000)
 
 
-def draw_window(p1, p2, turn):
+def draw_window(p1, p2, line, line2, turn):
     # draw background
     WIN.blit(GAME_IMAGE, (0, 0))
     # draw player health bars
@@ -254,21 +207,6 @@ def draw_snow():
 
     pygame.display.update()
 
-def show_go_screen():
-    WIN.blit(MAIN_IMAGE, (0,0))
-    draw_text('GAME NAME', HEALTH_FONT, WHITE, WIN, 320, 100)
-    draw_text("A and D to move, Mouse to fire",HEALTH_FONT,WHITE, WIN, 200, 270)
-    draw_text("Press a key to begin",HEALTH_FONT, WHITE, WIN, 270, 350)
-    pygame.display.flip()
-    waiting = True
-    while waiting:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYUP:
-                waiting = False
-
 
 def main():
     click = False
@@ -317,10 +255,25 @@ def main():
 
 
 def game():
+    player1 = Player(700, 300, pygame.image.load(
+        os.path.join("images", "spaceship_red.png")))
+
+    player2 = Player(100, 300, pygame.image.load(
+        os.path.join("images", "spaceship_yellow.png")))
 
     if not audio_compat:
         MAIN_BG_SOUND.stop()
         GAME_BG_SOUND.play()
+    player_1_turn = True
+    active_player = player1
+    dormant_player = player2
+    x = 0
+    y = 0
+    time = 0
+    power = 0
+    angle = 0
+    shoot = False
+    hit_registered = False
 
 
     # Setup snow
@@ -330,44 +283,21 @@ def game():
             y = random.randrange(0, HEIGHT)
             snow_list.append([x, y])
 
-    
-    game_over = True
+    # control fps
+    clock = pygame.time.Clock()
     run = True
     while run:
-        
-        #Resets the gameloop 
-        if game_over:
-           show_go_screen()
-           game_over = False
-           player1 = Player(700, 300, pygame.image.load(
-                os.path.join("images", "spaceship_red.png")))
-           player2 = Player(100, 300, pygame.image.load(
-                os.path.join("images", "spaceship_yellow.png")))
-           all_sprites = pygame.sprite.Group()
-           player_1_turn = True
-           active_player = player1
-           dormant_player = player2
-           active_player.hp = 100
-           dormant_player.hp = 100
-           x = 0
-           y = 0
-           time = 0
-           power = 0
-           angle = 0
-           shoot = False
-           hit_registered = False
-
 
         # Update the snow flakes
         if (use_snow):
             draw_snow()
-        print(time, '\n')
+
         # this now uses state design pattern
         if shoot:
-            if active_player.bullet.y < 362 and not hit_registered:  # bullet is still within frame
-                active_player.bullet.time += 0.25
+            if active_player.bullet.y < 500:  # bullet is still within frame
+                time += 0.25
                 active_player.bullet.x, active_player.bullet.y = active_player.bullet.projectilePath(
-                    x, y)#, power, angle, time)  # recalculate bullet location
+                    x, y, power, angle, time)  # recalculate bullet location
             else:  # bullet has left frame boundary
                 shoot = False
                 hit_registered = False
@@ -378,21 +308,17 @@ def game():
                 temp = active_player
                 active_player = dormant_player
                 dormant_player = temp
-        
-        all_sprites.update()
 
         # position of the mouse
         pos = pygame.mouse.get_pos()
         # invisible line determining the angle of the projectile
-        line1 = [(active_player.x + 10, active_player.y + active_player.height // 2 - 2), pos]
-        #line2 = [(player2.x + player2.width - 8,
-                  #player2.y + player2.height // 2 - 2), pos]
+        line1 = [(player1.x + 10, player1.y + player1.height // 2 - 2), pos]
+        line2 = [(player2.x + player2.width - 8,
+                  player2.y + player2.height // 2 - 2), pos]
 
         clock.tick(FPS)
         click = False
-        
         for event in pygame.event.get():
-
             # press close window event
             if event.type == pygame.QUIT:
                 run = False
@@ -414,29 +340,33 @@ def game():
                         BULLET_FIRE_SOUND.play()
                     shoot = True
 
-                    x, y = active_player.new_bullet()
-                    active_player.bullet.time = 0
-                    active_player.bullet.power = power = math.sqrt(
+                    if player_1_turn:
+                        player1.new_bullet()
+                        x = player1.bullet.x
+                        y = player1.bullet.y
+                        time = 0
+                        power = math.sqrt(
                             (line1[1][1] - line1[0][1]) ** 2 + (line1[1][0] - line1[0][0]) ** 2) / 8
-                    active_player.bullet.angle = findAngle(pos, active_player, active_player.bullet)
+                        angle = findAngle(pos, player1, player1.bullet)
+                    else:
+                        player2.new_bullet()
+                        x = player2.bullet.x
+                        y = player2.bullet.y
+                        time = 0
+                        power = math.sqrt(
+                            (line2[1][1] - line2[0][1]) ** 2 + (line2[1][0] - line2[0][0]) ** 2) / 8
+                        angle = findAngle(pos, player2, player2.bullet)
 
             # not sure how these hit events are meant to happen; collision doesn't seem to work yet
             elif event.type == PLAYER1_HIT or event.type == PLAYER2_HIT:
                 if not hit_registered:
-                    dormant_player.hp -= 50
-                    if not audio_compat:
-                        BULLET_HIT_SOUND.play()
-                    expl = Explosion( dormant_player.rect.center, 'lg')
-                    all_sprites.add(expl)
+                    dormant_player.hp -= 10
                     hit_registered = True
 
-            #pygame.sprite.spritecollide(dormant_player, active_player.bullet, True)
-    
-            if active_player.hp <= 0:  # if active player's HP is 0, that means the other player killed them last turn 
-               #simple formula to generate winning text instead of layers of conditionals
-               draw_winner("PLAYER " + str(int(player_1_turn) + 1) + " WINS!")
-               game_over=True
-
+            if active_player.hp <= 0:  # if active player's HP is 0, that means the other player killed them last turn
+                # simple formula to generate winning text instead of layers of conditionals
+                draw_winner("PLAYER " + str(int(player_1_turn) + 1) + " WINS!")
+                break
 
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_a]:
@@ -445,12 +375,9 @@ def game():
             active_player.move_right()
 
         handle_bullets(active_player, dormant_player, player_1_turn)
-        draw_window(player1, player2, int(player_1_turn) + 1)
-       
+        draw_window(player1, player2, line1, line2, int(player_1_turn) + 1)
         pygame.draw.rect(WIN, (255, 0, 0), player1.rect, 2)
         pygame.draw.rect(WIN, (255, 0, 0), player2.rect, 2)
-
-        all_sprites.draw(WIN)
 
     pygame.quit()
 
